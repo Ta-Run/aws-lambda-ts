63cefb39d0ec847845ea7d900397909ff85d2cce
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-03 15:15:57 +0530

    add new repo

---
+++ dist/index.js
@@ -0,0 +1,52 @@
+/******/ // The require scope
+/******/ var __nccwpck_require__ = {};
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/define property getters */
+/******/ (() => {
+/******/ 	// define getter functions for harmony exports
+/******/ 	__nccwpck_require__.d = (exports, definition) => {
+/******/ 		for(var key in definition) {
+/******/ 			if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
+/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			}
+/******/ 		}
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/hasOwnProperty shorthand */
+/******/ (() => {
+/******/ 	__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
+/* harmony export */   "y": () => (/* binding */ handler)
+/* harmony export */ });
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// import * as _ from 'lodash'
+const handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    const max = 10;
+    // const val = _.random(max);
+    const response = {
+        statusCode: 200,
+        body: `The random value is ${max}` // Adjusted the message to be more meaningful
+    };
+    return response;
+});
+
+var __webpack_exports__handler = __webpack_exports__.y;
+export { __webpack_exports__handler as handler };
