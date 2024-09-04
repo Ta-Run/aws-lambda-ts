b2c8b5e05cd7f247307b0d6f6e3382afcd5909d0
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-04 15:47:27 +0530

    change mongodb connection

--- index.js
+++ index.js
@@ -5,40 +5,26 @@ let conn = null;
 const uri = 'mongodb+srv://tarunsharma11091999:wABPUZltxCpzKeQ6@cluster0.1driiqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 exports.handler = async function(event, context) {
-  // Make sure to add this so you can re-use `conn` between function calls.
-  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
   context.callbackWaitsForEmptyEventLoop = false;
 
-  // Because `conn` is in the global scope, Lambda may retain it between
-  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
-  // This means your Lambda function doesn't have to go through the
-  // potentially expensive process of connecting to MongoDB every time.
   if (conn == null) {
-    conn = mongoose.createConnection(uri, {
+    conn = mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // Buffering means mongoose will queue up operations if it gets
-      // disconnected from MongoDB and send them when it reconnects.
-      // With serverless, better to fail fast if not connected.
-      bufferCommands: false, // Disable mongoose buffering
-      // and tell the MongoDB driver to not wait more than 5 seconds
-      // before erroring out if it isn't connected
+      bufferCommands: false,
       serverSelectionTimeoutMS: 5000
     });
-
-    // `await`ing connection after assigning to the `conn` variable
-    // to avoid multiple function calls creating new connections
     await conn;
-    conn.model('Test', new mongoose.Schema({ name: String }));
   }
 
-  const M = conn.model('Test');
+  // Define your model only if it's not already defined
+  const M = mongoose.models.Test || mongoose.model('Test', new mongoose.Schema({ name: String }));
 
   const doc = await M.findOne();
   console.log(doc);
 
   return {
-    status:200,
-    body:JSON.stringify('connect with mongodb')
+    statusCode: 200,
+    body: JSON.stringify('Connected to MongoDB'),
   };
 };
