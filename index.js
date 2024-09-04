5edd95c95d172bcde91cc45ef95a37e098baf708
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-04 15:30:22 +0530

    add mongoose collection

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+import mongoose from "mongoose";
 
+let conn = null;
 
-export async function handler(event, context) {
+const uri = 'mongodb+srv://tarunsharma11091999:wABPUZltxCpzKeQ6@cluster0.1driiqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
+exports.handler = async function(event, context) {
+  // Make sure to add this so you can re-use `conn` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
   context.callbackWaitsForEmptyEventLoop = false;
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('lambda function is start'),
-  };
+  // Because `conn` is in the global scope, Lambda may retain it between
+  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+  // This means your Lambda function doesn't have to go through the
+  // potentially expensive process of connecting to MongoDB every time.
+  if (conn == null) {
+    conn = mongoose.createConnection(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      // and tell the MongoDB driver to not wait more than 5 seconds
+      // before erroring out if it isn't connected
+      serverSelectionTimeoutMS: 5000
+    });
 
-  return response;
-}
+    // `await`ing connection after assigning to the `conn` variable
+    // to avoid multiple function calls creating new connections
+    await conn;
+    conn.model('Test', new mongoose.Schema({ name: String }));
+  }
+
+  const M = conn.model('Test');
+
+  const doc = await M.findOne();
+  console.log(doc);
+
+  return doc;
+};
