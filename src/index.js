52f273eca328969b5f62cb74996b1efd8e1a9ba5
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-09 12:48:48 +0530

    add index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import { connectToDatabase } from "./database/db.js";
-import { routeHandler } from "./routes/productRoutes.js";
+// import { connectToDatabase } from "./database/db.js";
+// import { routeHandler } from "./routes/productRoutes.js";
 
 export const handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  await connectToDatabase();
+  // await connectToDatabase();
 
-  return await routeHandler(event);
+  // return await routeHandler(event);
+  return resposne = {
+    status:200,
+    body:JSON.stringify('it is work')
+
+  }
 };
